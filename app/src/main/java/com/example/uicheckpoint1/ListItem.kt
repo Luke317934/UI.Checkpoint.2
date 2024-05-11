15bc1a9ce00c9232ee28ff189ab4a9dd28483acf
Lukas <lukas_henrik.moll@smail.th-koeln.de>
2024-05-11 12:00:47 +0200

    UI Checkpoint 2

--- app/src/main/java/com/example/uicheckpoint1/ListItem.kt
+++ app/src/main/java/com/example/uicheckpoint1/ListItem.kt
@@ -1,10 +1,7 @@
 package com.example.uicheckpoint1
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
